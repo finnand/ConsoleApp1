9ebc8e4574745aff1c83f44a55dee2403cdef76b
Andersen.Finn FIA <FIA@kmd.dk>
2018-08-28 10:11:54 +0200

    gdhf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilTyper ft = new FilTyper();
+            ft = FilTyper.csv;
+
+            Console.WriteLine(value: $"Aktuel filtype er: {ft.ToString()}" );
+
+            Person p = new Person();
+            p.Id = 1;
+            p.Navn = "Mikkel";
+
+             Console.WriteLine($"Navnet på personen er: {p.Navn}");
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
