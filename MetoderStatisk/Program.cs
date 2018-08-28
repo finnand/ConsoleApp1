be590522e06622461492af24fc30534e4f82fe38
Andersen.Finn FIA <FIA@kmd.dk>
2018-08-28 13:56:47 +0200

    xxx

--- MetoderStatisk/Program.cs
+++ MetoderStatisk/Program.cs
@@ -11,14 +11,36 @@ namespace MetoderStatisk
         static void Main(string[] args)
         {
 
-            if (System.IO.File.Exists(@"c:\temp\test.txt")) {
-                Console.WriteLine("Hurra - filen blev fundet.");
+            //if (System.IO.File.Exists(@"c:\temp\test.txt")) {
+            //    Console.WriteLine("Hurra - filen blev fundet.");
+            //}
+            //else
+            //    Console.WriteLine("Filen eksisterer ikke.");
+
+
+            string sti = "c:\\temp\\tal.txt";
+            //if (!System.IO.File.Exists(sti))
+            //    throw new ApplicationException("Filen c:\\temp\\tal.txt findes ikke!");
+
+            string indhold = "";
+            try
+            {
+                indhold = System.IO.File.ReadAllText(sti);
+                int tal = Convert.ToInt32(indhold);
+                tal++;
+                System.IO.File.WriteAllText(sti, tal.ToString());
+                Console.WriteLine($"Tal i fil er nu {tal}");
             }
-            else
-                Console.WriteLine("Filen eksisterer ikke.");
+            catch (System.FormatException)
+            {
+                throw new ApplicationException($"'{indhold}' kan ikke konverteres til et heltal");
 
+            }
+            catch (Exception)
+            {
+                throw;
+            }
 
-            
 
 
 
